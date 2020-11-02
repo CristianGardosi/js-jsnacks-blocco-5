67595f1e7b76decbfe77a11789a6beb722691619
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-11-02 17:03:43 +0100

    SOME CSS

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
 // Creare un array di oggetti
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore con handlebars.
-
 var bikes = [
     {
         nome: 'Pinarello',
@@ -60,6 +59,12 @@ $('.bike-container').append(html);
 // Partendo da un array di nomi (scelti da voi) creiamo un nuovo array.
 // Chiedere due numeri all'utente per definire la posizione iniziale e finale dal quale estrarre i valori dall'array originale.
 // Il nuovo array deve contenere gli elementi dell'array originale aventi la posizione (indice array)  compresa tra i due numeri inseriti dall'utente.
+
+
+
+
+
+
 
 // JSnack 3 ******************************************
 // Creiamo un array di oggetti (scelti da voi)
