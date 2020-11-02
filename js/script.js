f9fff88109d576931ce15659840e22751140a90e
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-11-02 17:21:30 +0100

    2/3

--- js/script.js
+++ js/script.js
@@ -59,6 +59,22 @@ $('.bike-container').append(html);
 // Partendo da un array di nomi (scelti da voi) creiamo un nuovo array.
 // Chiedere due numeri all'utente per definire la posizione iniziale e finale dal quale estrarre i valori dall'array originale.
 // Il nuovo array deve contenere gli elementi dell'array originale aventi la posizione (indice array)  compresa tra i due numeri inseriti dall'utente.
+var arrayNomi = [ 'Luca', 'Matteo', 'Mirko', 'Giacomo', 'Andrea', 'Giuseppe' ];
+// Necessario indicare il -1 in quanto array parte da 0 e length da 1
+var lunghezzaArray = arrayNomi.length - 1;
+// Imposto i due prompt di richiesta range all'utente
+var min = parseInt( prompt('Inserisci il valore di inizio range compreso tra 0 e ' + lunghezzaArray) );
+var max = parseInt( prompt('Inserisci il valore di fine range compreso tra 0 e ' + lunghezzaArray) );
+// Notiamo che è possibile che l'utente inserisca un valore minimo e massimo uguali (il che ci darà come risultato un nuovo array popolato da un solo nome)
+var newArray = [];
+
+for (var i = 0; i < arrayNomi.length; i++) {
+    // Se il valore di min è inferiore o uguale al valore indicato in max (e viceversa) allora eseguiamo il push e popoliamo dinamicamente il nuovo array
+    if ( min <= i && max >= i ) {
+        newArray.push(arrayNomi[i]);
+    }
+};
+console.log(newArray);
 
 
 
@@ -70,6 +86,7 @@ $('.bike-container').append(html);
 // Creiamo un array di oggetti (scelti da voi)
 // Creiamo una copia da questo array di partenza e aggiungiamo ai singoli elementi una nuova proprietà position che contiene una lettera casuale.
 // Non dobbiamo modificare l'array iniziale.
+
 
 
 
